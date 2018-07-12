161e715b0af37c917246425973889111da22adca
uestczhb <hongbo.zhang83@gmail.com>
2018-07-13 00:02:46 +0800

    Add set simulation envirement empty file template

---
+++ setsimenv.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Description:
+    SETSIMENV Set simulation envirement.
+
+EXAMPLE:
+    setsimenv
+    
+INPUT:
+    
+    
+OUTPUT:
+    
+ 
+ Copyright, 2018 (C), H.B. Zhang, <hongbo.zhang83@gmail.com>
+
+Modifications:
+Version    Date        Author        Log.
+V1.0       20180712    H.B. Zhang    Create this script
+Ref:
+
+"""
+
